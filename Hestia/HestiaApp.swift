5d52052de656bcffb84738ce617d609ddd0914b2
Matteo <37337358+Nexuss05@users.noreply.github.com>
2024-05-03 09:52:15 +0200

    first commit

---
+++ Hestia/HestiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HestiaApp.swift
+//  Hestia
+//
+//  Created by Matteo Cotena on 03/05/24.
+//
+
+import SwiftUI
+
+@main
+struct HestiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
